7299b27632de78067da188d4670366bdf72e51ec
David Little <littledavid175@gmail.com>
2019-08-17 22:48:43 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -49,7 +49,13 @@ class Song
   end
   
   def self.new_from_filename(name)
-    
+     song_name = name.split(" - ")[1]
+    artist_name = name.split(" - ")[0]
+    genre_name = name.split(" - ")[2].chomp(".mp3")
+     song = self.find_or_create_by_name(song_name)
+     song.artist = Artist.find_or_create_by_name(artist_name)
+     song.genre = Genre.find_or_create_by_name(genre_name)
+    song
   end
   
 end
