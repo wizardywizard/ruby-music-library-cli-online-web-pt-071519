afc1b9060399617fe663dc8241e9104d9bd7ee5e
David Little <littledavid175@gmail.com>
2019-08-24 23:03:27 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -41,10 +41,6 @@ class MusicLibraryController
   end
 
   def list_songs
-    # Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-    #     puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-    #   end
-    #binding.pry
     Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
